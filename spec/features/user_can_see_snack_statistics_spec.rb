0f24ea11f82182e7790f876b79a830592b01dd2d
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-26 11:05:09 -0600

    I realized my schema was poor in the last 10 minutes, so the final test has notes for what I should have done

--- spec/features/user_can_see_snack_statistics_spec.rb
+++ spec/features/user_can_see_snack_statistics_spec.rb
@@ -13,6 +13,17 @@ feature 'When a user visits a snack show page' do
     MachineItem.create(machine: @machine1, item: @item2)
   end
 
-  
+  # JUST REALIZED MachineItem should hold price -- it's 10:55, due in 10 minutes....
+
+  # need to remove Item's price field
+  # need to add column 'machine_price' (aka price) to MachineItem
+  #  - need an Item joins MachineItems
+  #something like:  Item.select('*, AVG(machine_price) AS average_price').group(id, MachineItem.item_id)
+  # ^^ gives you average price for that item across all machines
+  # need to make a join for average price (and count) of all items in a machine
+  #something like:  Machine.select('*, AVG(machine_price) AS average_price, COUNT( !!!!!!)').group(id, MachineItem.item_id)
+                                                                              # ^ do we need stock qty or all items in machine? 
+
+
 
 end
