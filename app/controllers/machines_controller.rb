165bcb16bb359ed738346858e936f45f01ebc2fa
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-26 10:38:58 -0600

    user story 1

--- app/controllers/machines_controller.rb
+++ app/controllers/machines_controller.rb
@@ -1,9 +1,11 @@
 class MachinesController < ApplicationController
+
   def index
     @owner = Owner.find(params[:owner_id])
   end
 
   def show
     @machine = Machine.find(params[:id])
+    @items  = @machine.items
   end
 end
--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -1,12 +1,36 @@
 require 'rails_helper'
 
 feature 'When a user visits a vending machine show page' do
+
+  before(:each) do
+    @owner1 = Owner.create(name: "Sam's Snacks")
+    @machine1  = @owner1.machines.create(location: "Don's Mixed Drinks")
+    @item1 = Item.create(name: "Snack 1", price: 1)
+    @item2 = Item.create(name: "Snack 2", price: 2)
+    MachineItem.create(machine: @machine1, item: @item1)
+    MachineItem.create(machine: @machine1, item: @item2)
+  end
+
+
   scenario 'they see the location of that machine' do
-    owner = Owner.create(name: "Sam's Snacks")
-    dons  = owner.machines.create(location: "Don's Mixed Drinks")
-
-    visit machine_path(dons)
-
+    visit machine_path(@machine1)
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
   end
+
+  scenario 'they see all snacks in that machine with name and price' do
+    visit machine_path(@machine1)
+    expect(page).to have_content('Snack 1')
+    expect(page).to have_content('$1')
+    expect(page).to have_content('Snack 2')
+    expect(page).to have_content('$2')
+
+
+
+  end
+
+
+
+
+
+
 end
